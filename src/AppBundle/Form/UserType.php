16e008354760650120932ad7970a28f703896f3d
Guizmox44 <g.potiron@hotmail.fr>
2018-05-25 11:30:42 +0200

    form signin + scss

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -2,9 +2,17 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotBlankValidator;
 
 class UserType extends AbstractType
 {
@@ -14,12 +22,31 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('city')
-            ->add('zipcode')
-            ->add('email')
-            ->add('password')
-            ->add('picture');
+            ->add('username', null, [
+                'label' => 'Pseudo'
+            ])
+            ->add('city', null, [
+                'label' => 'Ville'
+            ])
+            ->add('zipcode', null, [
+                'label' => 'Code postal'
+            ])
+            ->add('email', RepeatedType::class, [
+                'type' => EmailType::class,
+                'first_options' => ['label' => 'Email'],
+                'second_options' => ['label' => 'Confirmer l\'email'],
+                'invalid_message' =>'L\'email saisi ne correspond pas',
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmer le mot de passe'],
+                'invalid_message' =>'Les mots de passe ne correspondent pas',
+            ])
+            ->add('agreeTerms', CheckboxType::class, array('mapped' => false,
+                'label' => ' J\' accepte les conditions générales d\'utilisation',
+                'constraints' => array(new NotBlank(array('message' => 'Veuillez accepter les conditions générales d\'utilisation'))
+                )));
     }/**
      * {@inheritdoc}
      */
