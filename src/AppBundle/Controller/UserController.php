93368b76983b9d6a2000abf073abdd30df40e810
Guillaume <34102980+Guizmox44@users.noreply.github.com>
2018-06-20 11:29:19 +0200

    Update UserController.php

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -10,6 +10,7 @@ use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class UserController extends Controller
 {
@@ -37,5 +38,22 @@ class UserController extends Controller
         return $this->render('user/signin.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+    
+     /**
+     * @Route("/login", name="login")
+     */
+    public function loginAction(Request $request AuthenticationUtils $authenticationUtils) 
+    {
+         // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('user/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ));
     }
 }
